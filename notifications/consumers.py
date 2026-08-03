385510657245cf4df503712f368c0e264258c233
Santosh B <sbohara579@gmail.com>
2026-08-03 23:26:33 +0545

    web socket working solved

--- notifications/consumers.py
+++ notifications/consumers.py
@@ -6,53 +6,47 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class NotificationConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        self.user = self.scope['user']
+        self.user = self.scope["user"]
 
+        # Reject unauthenticated users
         if self.user.is_anonymous:
             await self.close()
             return
 
-        self.group_name = f'user_{self.user.id}'
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        # Each user gets their own private notification room
+        self.group_name = f"user_{self.user.id}"
+
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+
         await self.accept()
 
         await self.send(text_data=json.dumps({
-            'type': 'connection_established',
-            'message': f'Welcome {self.user.username}!',
+            "event": "connection",
+            "message": "WebSocket connected successfully"
         }))
+
 
     async def disconnect(self, close_code):
-        if hasattr(self, 'group_name'):
-            await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
-    async def receive(self, text_data):
-        data = json.loads(text_data)
-        await self.send(text_data=json.dumps({
-            'type': 'echo',
-            'message': data.get('message'),
-        }))
+        if hasattr(self, "group_name"):
+            await self.channel_layer.group_discard(
+                self.group_name,
+                self.channel_name
+            )
+
 
     async def send_notification(self, event):
+
         await self.send(text_data=json.dumps({
-            'type': 'notification',
-            'id': event['id'],
-            'title': event['title'],
-            'body': event['body'],
-            'timestamp': event['timestamp'],
+            "event": "notification",
+            "data": {
+                "id": event["id"],
+                "title": event["title"],
+                "body": event["body"],
+                "created_at": event["created_at"],
+                "is_read": event.get("is_read", False),
+            }
         }))
-        
-# from channels.generic.websocket import AsyncWebsocketConsumer
-
-
-# class NotificationConsumer(AsyncWebsocketConsumer):
-
-#     async def connect(self):
-#         print("WebSocket connection received")
-
-#         await self.accept()
-
-#         print("WebSocket connection accepted")
-
-
-#     async def disconnect(self, close_code):
-#         print("WebSocket disconnected")
